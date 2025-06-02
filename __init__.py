17bc2b89ebffa478b38176bf1431fb59f5c23811
Richard Dixey <Richard.Dixey@diamond.ac.uk>
2025-06-02 11:56:37 +0100

    added init

---
+++ __init__.py
@@ -0,0 +1,5 @@
+"""A place for plans controlling the panda triggering."""
+
+from .ncd_panda import configure_panda_triggering, run_panda_triggering
+
+__all__ = ["configure_panda_triggering", "run_panda_triggering"]
