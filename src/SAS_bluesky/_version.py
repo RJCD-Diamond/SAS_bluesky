6cd4a8b7fb033f7f7b83a43ba3f9f9b9a86229fb
rjcd <rjcd@localhost.localdomain>
2025-06-04 10:24:06 +0100

    updated depedencies

--- src/SAS_bluesky/_version.py
+++ src/SAS_bluesky/_version.py
@@ -17,5 +17,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.dev56+g48aa60b.d20250604'
-__version_tuple__ = version_tuple = (0, 1, 'dev56', 'g48aa60b.d20250604')
+__version__ = version = '0.1.dev57+g3b91829.d20250604'
+__version_tuple__ = version_tuple = (0, 1, 'dev57', 'g3b91829.d20250604')
